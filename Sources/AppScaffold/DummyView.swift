7965981e2be02e8abc4d7e8910737978a64fe29d
Peter Szucs <frozenbubble@outlook.com>
2024-12-15 22:00:46 +0100

    dummy view

---
+++ Sources/AppScaffold/DummyView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+public struct DummyView: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 13.0.0, *)
+#Preview {
+    DummyView()
+}
