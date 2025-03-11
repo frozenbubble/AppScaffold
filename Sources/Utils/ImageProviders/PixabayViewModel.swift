b849f680b7fbab7000ca70012a1b2770f682ba07
Peter Szucs <frozenbubble@outlook.com>
2025-03-11 15:33:15 +0100

    add pixabay image provider

---
+++ Sources/Utils/ImageProviders/PixabayViewModel.swift
@@ -0,0 +1,79 @@
+import SwiftUI
+import Resolver
+
+import AppScaffoldCore
+
+public struct PixabayResultSet: Codable {
+    public let total, totalHits: Int
+    public let hits: [PixabayImageResult]
+}
+
+public struct PixabayImageResult: Codable, Identifiable {
+    public let id: Int
+    public let pageURL: String
+    public let type: PixabayTypeEnum
+    public let tags: String
+    public let previewURL: String
+    public let previewWidth, previewHeight: Int
+    public let webformatURL: String
+    public let webformatWidth, webformatHeight: Int
+    public let largeImageURL: String
+    public let imageWidth, imageHeight, imageSize, views: Int
+    public let downloads, collections, likes, comments: Int
+    public let userID: Int
+    public let user: String
+    public let userImageURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, pageURL, type, tags, previewURL, previewWidth, previewHeight, webformatURL, webformatWidth, webformatHeight, largeImageURL, imageWidth, imageHeight, imageSize, views, downloads, collections, likes, comments
+        case userID = "user_id"
+        case user, userImageURL
+    }
+}
+
+public enum PixabayTypeEnum: String, Codable {
+    case photo = "photo"
+    case illustration = "illustration"
+}
+
+@available(iOS 17.0, *)
+@Observable
+public class PixabayViewModel {
+    @Injected @ObservationIgnored var networkImageService: NetworkDownloader
+    
+    public var inProgress = false
+    public var images: [PixabayImageResult] = []
+    
+    public func search(_ searchTerm: String) async {
+        withAnimation { inProgress = true }
+        defer { withAnimation { inProgress = false } }
+        
+        var urlBuilder = URLComponents()
+        let key = URLQueryItem(name: "key", value: "43182366-0818ace92dc51858a73b0d6a2")
+        let q = URLQueryItem(name: "q", value: searchTerm)
+        let type = URLQueryItem(name: "image_type", value: "photo")
+        urlBuilder.scheme = "https"
+        urlBuilder.host = "pixabay.com"
+        urlBuilder.path = "/api/"
+        urlBuilder.queryItems = [key, q, type]
+        
+        guard let url = urlBuilder.url else {
+            return
+        }
+        
+        let resultSet: PixabayResultSet? = await networkImageService.fetchData(from: url)
+        images = resultSet?.hits ?? []
+    }
+    
+    public func downloadImage(_ image: PixabayImageResult) async -> UIImage? {
+        await networkImageService.downloadImage(from: image.largeImageURL)
+    }
+}
+
+public extension AppScaffold {
+    @available(iOS 17.0, *)
+    func usePixabay() {
+        Resolver.register { NetworkDownloader() }.scope(.shared)
+        Resolver.register { PixabayViewModel() }.scope(.shared)
+    }
+}
---
+++ Sources/Utils/Networking/NetworkDownloader.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+public class NetworkDownloader {
+    public func downloadImage(from urlString: String) async -> UIImage? {
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return nil
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Failed to load image: HTTP response not OK")
+                return nil
+            }
+            
+            guard let image = UIImage(data: data) else {
+                print("Failed to create UIImage from data")
+                return nil
+            }
+            
+            return image
+            
+        } catch {
+            print("Error downloading image: \(error)")
+            return nil
+        }
+    }
+    
+    public func fetchData<T: Decodable>(from urlStr: String) async -> T? {
+        guard let url = URL(string: urlStr) else {
+            print("Invalid URL")
+            return nil
+        }
+        
+        return await fetchData(from: url)
+    }
+    
+    public func fetchData<T: Decodable>(from url: URL) async -> T? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("Request failed")
+                return nil
+            }
+            
+            let decoder = JSONDecoder()
+            let decodedResponse = try decoder.decode(T.self, from: data)
+            return decodedResponse
+            
+        } catch {
+            print("Failed to fetch or decode data: \(error)")
+            return nil
+        }
+    }
+}
