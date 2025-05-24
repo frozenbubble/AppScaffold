9ab770b883085c550ef9f308e1558733015e5366
Peter Szucs <frozenbubble@outlook.com>
2025-05-24 15:00:29 +0200

    remove macosspecific sandbox stuff

--- Sources/Core/Constants/StorageKeys.swift
+++ Sources/Core/Constants/StorageKeys.swift
@@ -3,15 +3,15 @@ public enum AppScaffoldStorageKeys {
 //    public static let actions = "actions"
 //    public static let displayReviewRequest = "displayReviewRequest"
 //    public static let appTheme = "appTheme"
-    
+
     public static var reviewRequests: String { Self.prefixedKey("reviewRequests") }
-    
+
     public static var actions: String { Self.prefixedKey("actions") }
-    
+
     public static var displayReviewRequest: String { Self.prefixedKey("displayReviewRequest") }
-    
+
     public static var appTheme: String { Self.prefixedKey("appTheme") }
-    
+
     static func prefixedKey(_ key: String) -> String {
         if AppScaffold.defaultsPrefix.isEmpty {
             key
@@ -19,6 +19,6 @@ public enum AppScaffoldStorageKeys {
             "\(AppScaffold.defaultsPrefix).\(key)"
         }
     }
-    
+
     public static var needsOnboarding: String { Self.prefixedKey("needsOnboarding") }
 }
--- Sources/UI/Sandbox.swift
+++ Sources/UI/Sandbox.swift
@@ -1,47 +1,47 @@
-import SwiftUI
-
-struct VisualEffectView: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSVisualEffectView {
-        let effectView = NSVisualEffectView()
-        effectView.state = .active
-        return effectView
-    }
-    
-    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {}
-}
-
-private struct SandboxView: View {
-    var body: some View {
-        VStack {
-            Rectangle()
-                .fill(
-                    .blue
-                )
-            
-            Rectangle()
-                .fill(
-                    .blue
-                )
-        }
-    }
-}
-
-struct SandboxPreview: View {
-    @State var isPresented = false
-    
-    var body: some View {
-        ZStack {
-            Image(systemName: "person")
-                .resizable()
-                .scaledToFit()
-            Rectangle()
-                .fill(.clear)
-                .background(VisualEffectView().ignoresSafeArea())
-        }
-    }
-}
-
-#Preview {
-    SandboxPreview()
-        .frame(width: 400, height: 400)
-}
+//import SwiftUI
+//
+//struct VisualEffectView: NSViewRepresentable {
+//    func makeNSView(context: Context) -> NSVisualEffectView {
+//        let effectView = NSVisualEffectView()
+//        effectView.state = .active
+//        return effectView
+//    }
+//    
+//    func updateNSView(_ nsView: NSVisualEffectView, context: Context) {}
+//}
+//
+//private struct SandboxView: View {
+//    var body: some View {
+//        VStack {
+//            Rectangle()
+//                .fill(
+//                    .blue
+//                )
+//            
+//            Rectangle()
+//                .fill(
+//                    .blue
+//                )
+//        }
+//    }
+//}
+//
+//struct SandboxPreview: View {
+//    @State var isPresented = false
+//    
+//    var body: some View {
+//        ZStack {
+//            Image(systemName: "person")
+//                .resizable()
+//                .scaledToFit()
+//            Rectangle()
+//                .fill(.clear)
+//                .background(VisualEffectView().ignoresSafeArea())
+//        }
+//    }
+//}
+//
+//#Preview {
+//    SandboxPreview()
+//        .frame(width: 400, height: 400)
+//}
